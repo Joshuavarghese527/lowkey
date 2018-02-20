7f212b1abf67616820a58708e063b361dce44bbf
Joshuavarghese527 <joshuavarghese527@gmail.com>
2018-02-20 00:41:27 -0500

    First Commit

---
+++ android/app/src/main/java/com/lowkeyfinal/MainActivity.java
@@ -0,0 +1,17 @@
+package com.lowkeyfinal;
+
+import android.os.Bundle; // here
+import com.facebook.react.ReactActivity;
+// react-native-splash-screen >= 0.3.1
+import org.devio.rn.splashscreen.SplashScreen; // here
+// react-native-splash-screen < 0.3.1
+import com.cboy.rn.splashscreen.SplashScreen; // here
+
+public class MainActivity extends ReactActivity {
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
+    // ...other code
+}
